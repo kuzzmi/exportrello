158b9af9595d405be99416a9e7f3fadd94e926ba
Igor Kuzmenko <igor@kuzzmi.com>
2017-08-19 20:44:47 +0200

    Adds simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, './client/dist'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: [
+                path.resolve(__dirname, './client/src')
+            ],
+            options: {
+                presets: ['react']
+            },
+        }],
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './client/dist'),
+        compress: true,
+        port: 8000,
+        historyApiFallback: true,
+    }
+};
