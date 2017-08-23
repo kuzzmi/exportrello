50a4eea35645f52995659007a35518e0a0e31301
Igor Kuzmenko <igor@kuzzmi.com>
2017-08-23 02:09:43 +0200

    Adds options to QS of Trello API

--- server/trello.js
+++ server/trello.js
@@ -5,14 +5,15 @@ const { API_KEY } = process.env;
 // Trello API
 
 module.exports = {
-    get({ oauth_token, endpoint }) {
+    get({ oauth_token, endpoint, options }) {
         return request({
             uri: `https://api.trello.com/1/${endpoint}`,
             qs: {
                 key: API_KEY,
                 token: oauth_token,
+                ...options,
             },
             json: true,
         });
     },
-}
+};
